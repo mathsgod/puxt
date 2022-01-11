deabfd8f5ce9130c7d9b82322dd6627abdec7fa6
Raymond Chong <mathsgod@yahoo.com>
2022-01-11 16:27:05 +0800

    init

---
+++ hl-form/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h4>responsive hl-form</h4>
+    <hl-form :model="form1" ref="form1">
+      <el-form-item label="HL Form label" prop="input1" required>
+        <el-input v-model="form1.input1"></el-input>
+      </el-form-item>
+      <el-button @click="submit1">Submit</el-button>
+    </hl-form>
+
+    <el-divider></el-divider>
+
+    <el-form :model="form2" ref="form2">
+      <el-form-item label="EL Form label" prop="input1" required>
+        <el-input v-model="form2.input1"></el-input>
+      </el-form-item>
+      <el-button @click="submit2">Submit</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import HLForm from "./Form";
+export default {
+  name: "App",
+  components: {
+    "hl-form": HLForm,
+  },
+  data() {
+    return {
+      form1: {},
+      form2: {},
+    };
+  },
+  methods: {
+    submit1() {
+      this.$refs.form1.validate();
+    },
+    submit2() {
+      this.$refs.form2.validate();
+    },
+  },
+};
+</script>
---
+++ hl-form/src/Form.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  props: {
+    labelWidth: {
+      type: String,
+      default: "auto",
+    },
+  },
+  data() {
+    return {
+      labelPosition: "top",
+    };
+  },
+  render(h) {
+    this.labelPosition = window.innerWidth < 768 ? "top" : "left";
+    window.addEventListener("resize", () => {
+      this.labelPosition = window.innerWidth < 768 ? "top" : "left";
+    });
+    return h(
+      "el-form",
+      {
+        props: {
+          labelPosition: this.labelPosition,
+          labelWidth: this.labelWidth,
+        },
+        on: this.$listeners,
+        ref: "form",
+        attrs: this.$attrs,
+      },
+      this.$slots.default
+    );
+  },
+  methods: {
+    validate(callback) {
+      return this.$refs.form.validate(callback);
+    },
+    resetFields() {
+      return this.$refs.form.resetFields();
+    },
+    clearValidate() {
+      return this.$refs.form.clearValidate(arguments[0]);
+    },
+    validateField() {
+      return this.$refs.form.validateField(arguments[0]);
+    },
+  },
+};
+</script>
