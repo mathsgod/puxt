7f406e3e2cc29d1f87ad28ca53f56e33431ae53e
Raymond Chong <mathsgod@yahoo.com>
2022-07-19 16:31:48 +0800

    update

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+
+use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use PUXT\App;
+
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 require_once("vendor/autoload.php");
 
-(new PUXT\App)->run();
+(new SapiEmitter())->emit((new App)->handle(ServerRequestFactory::fromGlobals()));
