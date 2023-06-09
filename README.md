9d2de22af17e8b1e856f813713189c37d9296704
Raymond Chong <mathsgod@yahoo.com>
2023-06-09 16:05:56 +0800

    ✨ 功能(.env)：新增ROUTE_STRATEGY環境變數 📝 文件(README.md)：更新README文件，新增DEBUG_EXCEPTION_FORMAT和BASE_PATH環境變數的說明 🐛 修正(App.php)：修正base_path的設置，改為使用BASE_PATH環境變數 ✨ 功能(App.php)：新增對DEBUG_EXCEPTION_FORMAT和ROUTE_STRATEGY環境變數的支援

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # puxt
+
+## Debug
+Set the `DEBUG` environment variable to `true` to enable debug mode.
+```env
+DEBUG=true
+```
+
+### Exception format
+Set the `DEBUG_EXCEPTION_FORMAT` environment variable to `json` to enable exception format.
+```env
+DEBUG_EXCEPTION_FORMAT=json
+```
+
+### Base path of uri    
+Set the `BASE_PATH` environment variable to change the base path.
+```env
+
+
+## Route strategy
+Set the `ROUTE_STRATEGY` environment variable to change the route strategy.
+```env
+ROUTE_STRATEGY=json
+```
--- src/App.php
+++ src/App.php
@@ -28,9 +28,11 @@ use Twig\Loader\LoaderInterface;
 use Laminas\Di;
 use Laminas\Di\Container;
 use Laminas\Di\InjectorInterface;
+use Laminas\Diactoros\ResponseFactory;
 use Laminas\HttpHandlerRunner\RequestHandlerRunner;
 use Laminas\HttpHandlerRunner\RequestHandlerRunnerInterface;
 use Laminas\Stratigility\MiddlewarePipe;
+use League\Route\Strategy\JsonStrategy;
 use Psr\Http\Server\MiddlewareInterface;
 use Throwable;
 
@@ -111,41 +113,7 @@ class App implements EventDispatcherAware, LoggerAwareInterface, RequestHandlerR
             }
         );
 
-        $this->base = dirname($_SERVER["SCRIPT_NAME"]);
-        if ($this->base == "/") {
-            $this->base = "";
-        }
-        /* 
-        //create services manager
-
-
-        \Dotenv\Dotenv::createImmutable($root)->safeLoad();
-    
-        //base path
-        $this->base_path = dirname($_SERVER["SCRIPT_NAME"]);
-        if ($this->base_path == DIRECTORY_SEPARATOR) {
-            $this->base_path = "/";
-        }
-
-        if (substr($this->base_path, -1) != "/") {
-            $this->base_path .= "/";
-        }
-
-        $this->document_root = substr($this->root . "/", 0, -strlen($this->base_path));
-
-        //plugins
-        foreach ($this->config->get("plugins", []) as $plugin) {
-            if (file_exists($plugin)) {
-                $p = require($plugin);
-                if ($p instanceof Closure) {
-                    $context = $this->context;
-                    $inject = function (string $key, $value) use ($context) {
-                        $context->$key = $value;
-                    };
-                    $p->call($this, $context, $inject);
-                }
-            }
-        } */
+        $this->base = $_ENV["BASE_PATH"] ?? "/";
     }
 
     function getInjector(): InjectorInterface
@@ -178,86 +146,21 @@ class App implements EventDispatcherAware, LoggerAwareInterface, RequestHandlerR
 
     function handle(ServerRequestInterface $request): ResponseInterface
     {
-
-
-        /* 
-        if (strpos($request->getHeaderLine("Content-Type"), "application/json") !== false) {
-            $body = $request->getBody()->getContents();
-            $request = $request->withParsedBody(json_decode($body, true));
-            $_POST = $request->getParsedBody();
-        } */
-
-        //$this->context->_files = $request->getUploadedFiles();
-
-        /*     $path = $request->getUri()->getPath();
-
-        $request_path = substr($path, strlen($this->base_path));
-        if ($request_path === false) {
-            $request_path = "error";
-        }
- */
-        /*         $route = new Route();
-        $route->path = $request_path;
-        $route->query = $request->getQueryParams();
-        $route->params = new stdClass;
-
-        $this->context->route = $route;
-        $this->context->params = $route->params;
-        $this->context->query = $route->query;
-        $this->context->request = $request; */
-
-
         //load default router
         $router = $this->getRouter();
+
 
         try {
             $response = $router->dispatch($request);
         } catch (HttpException $e) {
-
-            $code = $e->getStatusCode();
-            if ($code < 100 || $code > 599) {
-                $code = 500;
-            }
-
-            if ($code == 500 && !$this->config->debug) {
-                $message = "Internal Server Error";
-            } else {
-                $message = $e->getMessage();
-            }
-
-            if ($this->config->debug_format == "html") {
-                return new HtmlResponse($message, $code);
-            } else {
-                return new JsonResponse([
-                    "error" => [
-                        "code" => $code,
-                        "message" => $message
-                    ]
-                ], $code);
-            }
+            return new HtmlResponse($e->getMessage(),  $e->getStatusCode());
         } catch (Exception $e) {
-            $code = $e->getCode();
-            if ($code < 100 || $code > 599) {
-                $code = 500;
-            }
-
+            $message = $e->getMessage();
             //hide message if debug is off and error is 500
-            if ($code == 500 && !$this->config->debug) {
+            if (!$_ENV["DEBUG"]) {
                 $message = "Internal Server Error";
-            } else {
-                $message = $e->getMessage();
             }
-
-            if ($this->config->debug_format == "html") {
-                return new HtmlResponse($message, $code);
-            } else {
-                return new JsonResponse([
-                    "error" => [
-                        "code" => $code,
-                        "message" => $message
-                    ]
-                ], $code);
-            }
+            return new HtmlResponse($message, 500);
         }
 
         if (
@@ -299,6 +202,11 @@ class App implements EventDispatcherAware, LoggerAwareInterface, RequestHandlerR
     {
         $router = new Router();
 
+        if ($_ENV["ROUTE_STRATEGY"] == "json") {
+            $strategy = new JsonStrategy(new ResponseFactory());
+            $router->setStrategy($strategy);
+        }
+
         $router->addPatternMatcher("any", ".+");
 
         $base_path = $this->root . DIRECTORY_SEPARATOR . "pages";
@@ -316,6 +224,7 @@ class App implements EventDispatcherAware, LoggerAwareInterface, RequestHandlerR
         })->map(function (DirectoryAttributes $attributes) use ($base_path) {
             return  ["path" => $attributes->path()];
         })->toArray();
+
 
         $data = [];
         foreach ($dirs as $dir) {
@@ -341,22 +250,23 @@ class App implements EventDispatcherAware, LoggerAwareInterface, RequestHandlerR
 
         $files = array_unique($files);
         foreach ($files as $s) {
-            $data["/" . $s] = $base_path . DIRECTORY_SEPARATOR . $s;
+            $data[$this->base . $s] = $base_path . DIRECTORY_SEPARATOR . $s;
         }
 
         //root index
         if ($fs->fileExists("index.php") || $fs->fileExists("index.html") || $fs->fileExists("index.twig")) {
-            $data["/"] = $base_path . DIRECTORY_SEPARATOR . "index";
+            $data[$this->base] = $base_path . DIRECTORY_SEPARATOR . "index";
         }
+
+
 
         $methods = ["GET", "POST", "PATCH", "PUT", "DELETE"];
         foreach ($data as $path => $file) {
             foreach ($methods as $method) {
                 $path = str_replace("@", ":", $path);
-                $router->map($method,   $this->base . $path, function (ServerRequestInterface $request, array $args) use ($file) {
+                $router->map($method,  $path, function (ServerRequestInterface $request, array $args) use ($file) {
                     $twig = $this->getTwig(new \Twig\Loader\FilesystemLoader([$this->root]));
                     $request = $request->withAttribute(\Twig\Environment::class, $twig);
-
                     return RequestHandler::Create($file)->handle($request);
                 });
             }
@@ -372,17 +282,23 @@ class App implements EventDispatcherAware, LoggerAwareInterface, RequestHandlerR
             $code = 500;
         }
 
-        $response = new JsonResponse([
-            "error" => [
-                "code" => $e->getCode(),
-                "message" => $e->getMessage(),
-                "line" => $e->getLine(),
-                "file" => $e->getFile(),
-                "trace" => $e->getTraceAsString()
+        $message = $_ENV["DEUBG"] ? $e->getMessage() : "Internal Server Error";
 
-            ],
-        ]);
-        $response = $response->withStatus($code, $e->getMessage());
+        if ($_ENV["DEBUG_EXCEPTION_FORMAT"] == "json") {
+            $response = new JsonResponse([
+                "error" => [
+                    "code" => $e->getCode(),
+                    "message" => $e->getMessage(),
+                    "line" => $e->getLine(),
+                    "file" => $e->getFile(),
+                    "trace" => $e->getTraceAsString()
+
+                ],
+            ]);
+            $response = $response->withStatus($code, $e->getMessage());
+        } else {
+            $response = new HtmlResponse($message . "<br><br><pre>" . $e->getTraceAsString() . "</pre>", 500);
+        }
 
         (new SapiEmitter())->emit($response);
     }
